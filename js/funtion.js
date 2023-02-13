5811fed0afc05ccf88ab1a586dd286a5e0945456
Jaquelin Brito <devjaquelinbrito@gmail.com>
2023-02-13 13:11:33 -0300

    mi primer archivo en el repositorio

---
+++ js/funtion.js
@@ -0,0 +1,13 @@
+document.getElementById("img1").addEventListener("mouseover", mouseOver);
+document.getElementById("img1").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("img1").style.background = "grey";
+}
+
+function mouseOut() {
+  document.getElementById("img1").style.background = "white"
+}
+
+
+ 
